501c0d059e028e53dcd7b79dbd40b8f20e80d464
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2023-05-11 11:00:20 -0700

    about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,7 @@
 import { request } from "@/lib/datocms";
 import { plexMono } from ".";
 import { FaUserAstronaut } from "react-icons/fa";
-import { FormEvent } from "react";
+import { AboutPage } from "@/components/about/const/interfaces";
 
 
 const ABOUT_QUERY = `
@@ -10,6 +10,9 @@ const ABOUT_QUERY = `
       aboutText
       id
       pageTitle
+    }
+    author {
+      authorname
     }
   }
 `
@@ -24,37 +27,36 @@ export async function getStaticProps() {
   }
 }
 
-interface AboutPageData {
-  id: string,
-  pageTitle: string,
-  aboutText: string,
-}
 
-interface AboutPage {
-  about: AboutPageData,
-}
 interface AboutPageProps {
   data: AboutPage,
 }
 
 export default function About({data}: AboutPageProps) {
-  console.log(data);
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-  }
+  const {author, about} = data ?? {};
   return (
     <main
       id='mainContent'
       className={`flex w-screen min-h-screen flex-col items-center md:justify-start gap-8${plexMono.className}`}
     >
-      <section className='flex flex-col w-full sm:w-9/12 md:w-5/6 xl:w-full xl:max-w-7xl sm:mx-auto items-center gap-8'>
-        <div className='w-full flex flex-col items-center p-2 xl:p-0 gap-8 mx-auto'>
-          <div className="flex flex-col items-center sm:flex-row lg:justify-center lg:mx-auto">
-            <div className='flex flex-col items-center justify-center sm:w-1/2 lg:w-1/4 lg:items-start lg:justify-start'>
-              <FaUserAstronaut className='text-7xl lg:text-9xl text-yellow-200'/>
+      <section className='flex flex-col w-full sm:w-9/12 md:w-5/6 xl:w-full xl:max-w-7xl sm:mx-auto items-center gap-8 p-8'>
+        <div className="flex flex-col items-center justify-center lg:mx-auto">
+          <div className='w-full flex flex-col sm:flex-row sm:justify-between items-center p-4'>
+            <div className="self-start w-1/2 py-6 sm:py-2">
+              <h1 className="text-yellow-200 text-5xl">{`Hello! I'm ${author.authorname}`}</h1>
+              <div className="hidden sm:flex sm:w-full sm:justify-center items-center sm:my-8">
+                <FaUserAstronaut className='inline-block w-60 h-60 text-text-white animate-wiggle' />
+              </div>
             </div>
-            <div className='flex flex-col items-center py-4 px-2 w-full sm:w-1/2'>
-              <article className='text-sm lg:text-md py-4 text-text-white' dangerouslySetInnerHTML={{__html: data.about.aboutText}}/>
+            <div className="w-full flex flex-col sm:w-1/2">
+              <article className='text-lg text-text-white' dangerouslySetInnerHTML={{__html: about.aboutText}}/>
+              <div>
+                <a className="w-full text-purple border-yellow-300 bg-yellow-300 border-2 rounded-lg p-6" 
+                href="mailto:robertgrunau@hey.com"
+                >
+                  reach out
+                </a>              
+              </div>
             </div>
           </div>
         </div>
