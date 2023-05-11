cb7676fe23881ba8bbbf07890ee6c616ed0177aa
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2023-05-11 01:10:02 -0700

    there we go

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,11 +4,16 @@ interface LayoutProps {
 }
 const Layout = ({ children }: LayoutProps) => (
   <>
-    <Head>
-      <title>robertgrunau.website</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
     <div className='flex flex-col gap-8 bg-purple'>
+      <Head>
+        <title>robertgrunau.website</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       {children}
     </div>
   </>
