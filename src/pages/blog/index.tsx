3914ef0dc1dd2cacaca27d22ce1a36e8fea001e0
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2023-05-14 20:08:59 -0700

    bigger layouts

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,6 +1,6 @@
 import { request } from '@/lib/datocms'
 import MainContainer from "@/components/common/MainContainer";
-import { Image, ResponsiveImageType } from 'react-datocms/image';
+import { Image } from 'react-datocms/image';
 import Link from 'next/link';
 import { CoverImage } from '@/components/home/const/interfaces';
 
@@ -17,24 +17,45 @@ const View = ({allBlogPosts, noPosts}: ViewProps) => (
           <h1>There are no posts yet</h1>
         </div>
       )}
-      {!noPosts && allBlogPosts.map((post) => (
-        <div
-          className='p-4 w-[90%] mx-auto md:w-1/2 lg:w-1/3 border-b-2 border-gray-200 text-text-white mb-6'
-          key={post.id}
-        >
-          <Link 
-            className='w-full flex flex-col gap-4'
-            href={`/blog/${post.slug}`}>
-            <Image data={post.postHero.responsiveImage} />
-            <div>
-              <h1 className='text-xl font-normal'>{post.title}</h1>
+      <div className='flex flex-col lg:grid lg:grid-cols-[1fr_250px] lg:grid-rows-2'>
+        <aside className='hidden lg:block lg:col-start-2 lg:col-end-3 lg:row-span-2 text-yellow-200'>
+          <div className='flex flex-col items-center justify-center text-center px-8 py-2 w-full'>
+            <div className='mt-8 mb-4 border-b-2 border-b-yellow-200'>
+              <h1 className='text-2xl'>All Posts</h1>
             </div>
-            <div className='text-md font-thin'>
-              <article dangerouslySetInnerHTML={{__html: post.excerpt}} />
+            <div className='flex flex-col'>
+              {allBlogPosts.map((post) => (
+                <Link 
+                  href={post.slug}
+                  className='text-left text-xl mx-8 my-2'
+                >
+                  <span>{post.title}</span>
+                </Link>
+              ))}
             </div>
-          </Link>
+          </div>
+        </aside>
+        <div className='flex flex-col items-center justify-center text-center lg:col-start-1 lg:col-end-2 row-start-1 row-end-3'>
+          {!noPosts && allBlogPosts.map((post) => (
+            <div
+              className='p-4 mx-auto md:w-11/12 lg:w-11/12 border-b-2 border-gray-200 text-text-white my-6'
+              key={post.id}
+            >
+              <Link 
+                className='w-full flex flex-col gap-4 text-left'
+                href={`/blog/${post.slug}`}>
+                <Image className='border-2 border-gray-200 rounded-lg' data={post.postHero.responsiveImage} />
+                <div className='px-4'>
+                  <h1 className='text-xl font-normal'>{post.title}</h1>
+                </div>
+                <div className='text-md font-thin '>
+                  <article className='px-4' dangerouslySetInnerHTML={{__html: post.excerpt}} />
+                </div>
+              </Link>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
   </MainContainer>
 );
 
@@ -95,7 +116,6 @@ interface BlogPageDataProps {
 const BlogIndex = ({data}: BlogPageDataProps) => { 
 
   const { allBlogPosts } = data ?? {};
-  console.log(data);
 
   const hookProps = {
     noPosts: allBlogPosts.length === 0,
