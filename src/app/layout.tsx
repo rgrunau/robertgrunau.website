f6220a94acbbfc6bf780178b03cd89262a36c9b5
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2023-09-10 20:17:12 -0700

    fucking with logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next"
-import '@/styles/globals.css'
+import './globals.css'
 import { IBM_Plex_Mono } from 'next/font/google'
 import Header from "@/components/layout/Header"
 import Footer from "@/components/layout/Footer"
@@ -14,12 +14,7 @@ const meta: Metadata = {
 export default function RootLayout ({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="description" content={meta.description || ""} />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-      </head>
+      
       <body className={`bg-purple ${plexMono.className}`}>
         <Header />
           { children}
