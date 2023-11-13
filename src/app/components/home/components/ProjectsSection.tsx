dd9ef886d360f123d5b89a6c6d1cbde676ab6081
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2023-11-13 15:24:29 -0800

    add projects section

--- src/app/components/home/components/ProjectsSection.tsx
+++ src/app/components/home/components/ProjectsSection.tsx
@@ -8,36 +8,49 @@ interface ProjectSectionProps {
 
 export default function ProjectsSection({ projects }: ProjectSectionProps) {
   return (
-    <div className="w-full">
-      <div className="w-full lg:w-3/4 mx-auto flex flex-col justify-center items-center py-5">
-        <div className="w-full">
-          <h2 className="text-3xl font-semibold">Featured Project</h2>
+    <section className="w-full max-w-[1350px] flex flex-col lg:flex-row lg:items-center px-4 py-8">
+      <div className="w-full flex flex-col items-start justify-start lg:justify-between">
+        <div>
+          <h2 className="text-5xl lg:text-7xl font-bold text-black">
+            Projects
+          </h2>
         </div>
-        <div className="w-full">
-          <ul className="w-full flex flex-col justify-start items-start my-4">
-            {projects?.map((project) => (
-              <li key={project._id}>
-                <Link
-                  href={`/projects/${project.slug}`}
-                  className="w-full flex flex-col justify-start items-start"
-                >
-                  <div className="w-full">
-                    <Image
-                      src={project.mainImage}
-                      alt={project.mainImageAlt}
-                      width={400}
-                      height={400}
-                    />
-                  </div>
-                  <div className="w-full p-2">
-                    <h4 className="text-2xl">{project.projectTitle}</h4>
-                  </div>
-                </Link>
-              </li>
-            ))}
-          </ul>
+        <div className="hidden lg:block my-6 py-2">
+          <Link
+            href="/work"
+            className="text-2xl font-light text-black bg-rg-orange hover:bg-white border-2 border-rg-orange px-4 py-2 rounded-lg"
+          >
+            See More
+          </Link>
         </div>
       </div>
-    </div>
+      <div className="w-full py-4">
+        <ul>
+          {projects.map((project) => (
+            <Link
+              key={project.slug}
+              href={`/work/${project.slug}`}
+              className="font-light text-black"
+            >
+              <div className="w-full flex flex-col">
+                <div className="w-full">
+                  <Image
+                    src={project.mainImage}
+                    alt={project.mainImageAlt}
+                    width={2336}
+                    height={1460}
+                  />
+                </div>
+                <div className="mt-3">
+                  <h3 className="text-3xl font-light text-black">
+                    {project.projectTitle}
+                  </h3>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </ul>
+      </div>
+    </section>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,6 @@ import {
 } from "@/sanity/sanity-queries";
 import BlogSection from "./components/home/components/BlogSection";
 import ProjectsSection from "./components/home/components/ProjectsSection";
-import Link from "next/link";
-import Image from "next/image";
 
 export default async function Home() {
   const data = await getHomePageData();
@@ -30,40 +28,7 @@ export default async function Home() {
           newsLetterTitle={data.newsletter_title}
           newsLetterBlurb={data.newsletter_blurb}
         />
-        <section className="w-full max-w-[1350px] flex flex-col lg:flex-row lg:items-center px-4 py-8">
-          <div className="w-full lg:flex items-center justify-start">
-            <h2 className="text-5xl lg:text-7xl font-bold text-black">
-              Projects
-            </h2>
-          </div>
-          <div className="w-full py-4">
-            <ul>
-              {projects.map((project) => (
-                <Link
-                  key={project.slug}
-                  href={`/work/${project.slug}`}
-                  className="font-light text-black"
-                >
-                  <div className="w-full flex flex-col">
-                    <div className="w-full">
-                      <Image
-                        src={project.mainImage}
-                        alt={project.mainImageAlt}
-                        width={2336}
-                        height={1460}
-                      />
-                    </div>
-                    <div className="mt-3">
-                      <h3 className="text-3xl font-light text-black">
-                        {project.projectTitle}
-                      </h3>
-                    </div>
-                  </div>
-                </Link>
-              ))}
-            </ul>
-          </div>
-        </section>
+        <ProjectsSection projects={projects} />
       </MainContainer>
     </>
   );
